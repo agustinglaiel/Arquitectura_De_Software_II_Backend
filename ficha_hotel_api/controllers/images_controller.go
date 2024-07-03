37b19ec9103ab5475e7c79709870cb4b53e6cdf9
Agustin Glaiel <agustinglaiel@gmail.com>
2024-07-02 22:21:22 -0300

    arreglos

--- ficha_hotel_api/controllers/images_controller.go
+++ ficha_hotel_api/controllers/images_controller.go
@@ -1,8 +1,9 @@
 package controllers
 
 import (
-	"busqueda_hotel_api/utils/errors"
 	"ficha_hotel_api/dtos"
+	se "ficha_hotel_api/services"
+	"ficha_hotel_api/utils/errors"
 	"io/ioutil"
 	"net/http"
 	"strconv"
@@ -17,7 +18,8 @@ func InsertImage(ctx *gin.Context) {
 	if contentType != "image/jpeg" && contentType != "image/png" {
 		errMsg := "Content-Type debe ser image/jpeg o image/png"
 		log.Error(errMsg)
-		apiErr := errors.NewBadRequestErrorApi(errMsg)
+		apiErr := errors.NewBadRequestApiError(errMsg)
+		
 		ctx.JSON(apiErr.Status(), apiErr)
 		return
 	}
@@ -25,28 +27,23 @@ func InsertImage(ctx *gin.Context) {
 	imagenBytes, err := ioutil.ReadAll(ctx.Request.Body)
 	if err != nil {
 		log.Error(err.Error())
-		apiErr := errors.NewInternalServerErrorApi("Error al leer el cuerpo de la solicitud", err)
+		apiErr := errors.NewBadRequestApiError("Error al leer el cuerpo de la solicitud")
 		ctx.JSON(apiErr.Status(), apiErr)
 		return
 	}
 
 	var img dtos.ImageDto
 	img.Data = imagenBytes
-	hotelId, err := strconv.Atoi(ctx.Param("idHotel"))
 
-	if err != nil {
-		log.Error(err.Error())
-		apiErr := errors.NewInternalServerErrorApi("Error al convertir el ID del hotel", err)
-		ctx.JSON(apiErr.Status(), apiErr)
-		return
-	}
-	img.HotelId = hotelId
+
+	
+	img.HotelId = ctx.Param("idHotel")
 
 	img, err = se.ImageService.InsertImage(img)
 
 	if err != nil {
 		log.Error(err.Error())
-		apiErr := errors.NewInternalServerErrorApi("Error al insertar la imagen", err)
+		apiErr := errors.NewBadRequestApiError("Error al insertar la imagen",)
 		ctx.JSON(apiErr.Status(), apiErr)
 		return
 	}
@@ -61,16 +58,16 @@ func GetImagesByHotelId(ctx *gin.Context) {
 	if err != nil {
 		errMsg := "ID de hotel inválido"
 		log.Error(errMsg)
-		apiErr := errors.NewBadRequestErrorApi(errMsg)
+		apiErr := errors.NewBadRequestApiError(errMsg)
 		ctx.JSON(apiErr.Status(), apiErr)
 		return
 	}
 
-	imagesDto, err := HotelService.GetImagesByHotelId(id)
+	imagesDto, err := se.ImageService.GetImagesByHotelId(id)
 
 	if err != nil {
 		log.Error(err.Error())
-		apiErr := errors.NewInternalServerErrorApi("Error al obtener imágenes por ID de hotel", err)
+		apiErr := errors.NewInternalServerApiError("Error al obtener imágenes por ID de hotel", err)
 		ctx.JSON(apiErr.Status(), apiErr)
 		return
 	}
