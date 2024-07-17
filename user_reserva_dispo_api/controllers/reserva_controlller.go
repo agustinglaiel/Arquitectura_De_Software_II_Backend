699cf51315ae55324f78a68e8da764f6a6de3560
Julian Ahumada <julianahumada86@gmail.com>
2024-07-16 23:02:19 -0300

    poniendo en comun

--- user_reserva_dispo_api/controllers/reserva_controlller.go
+++ user_reserva_dispo_api/controllers/reserva_controlller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"strconv"
 	"user_reserva_dispo_api/dtos"
 	"user_reserva_dispo_api/services"
 	"user_reserva_dispo_api/utils/errors"
@@ -11,28 +12,53 @@ import (
 func ComprobaDispReserva(c *gin.Context) {
 	var avalabilityReservationDto dtos.ReservationAvailabilityDto
 	var err errors.ApiError
-	c.Param("StartDate")
-	c.Param("EndDate")
-	c.Param("HotelID")
 
 	avalabilityReservationDto.StartDate, err = services.ReservaService.ParseDate(c.Param("StartDate"))
-
 	if err != nil {
-		//poner error
-
+		c.JSON(400, err)
 	}
-
 	avalabilityReservationDto.StartDate, err = services.ReservaService.ParseDate(c.Param("EndDate"))
-
 	if err != nil {
-		//poner error
-
+		c.JSON(400, err)
 	}
 	avalabilityReservationDto.HotelID = c.Param("HotelID")
 	if avalabilityReservationDto.HotelID == "" {
-		//poner error
+		c.JSON(400, err)
+	}
+	err = services.ReservaService.ComprobaDispReserva(avalabilityReservationDto)
+	if err != nil {
+		c.JSON(400, err)
+
+	}
+	c.JSON(200, nil)
+
+}
+
+func PostReserva(c *gin.Context) {
+	var avalabilityReservationDto dtos.CreateReservationDto
+	var err errors.ApiError
+
+	avalabilityReservationDto.StartDate, err = services.ReservaService.ParseDate(c.Param("StartDate"))
+	if err != nil {
+		c.JSON(400, err)
 	}
 
-	_, err = services.ReservaService.ComprobaDispReserva(avalabilityReservationDto)
+	avalabilityReservationDto.EndDate, err = services.ReservaService.ParseDate(c.Param("EndDate"))
+	if err != nil {
+		c.JSON(400, err)
+	}
+	avalabilityReservationDto.HotelID = c.Param("HotelID")
+
+	if err != nil {
+		c.JSON(400, err)
+	}
+	userId, errr := strconv.Atoi(c.Param("idUser"))
+	if errr != nil {
+		c.JSON(400, err)
+	}
+
+	avalabilityReservationDto.UserID = userId
+
+	services.ReservaService.PostReserva(avalabilityReservationDto)
 
 }
--- user_reserva_dispo_api/services/reserva_service.go
+++ user_reserva_dispo_api/services/reserva_service.go
@@ -9,6 +9,7 @@ import (
 	"time"
 	"user_reserva_dispo_api/daos"
 	"user_reserva_dispo_api/dtos"
+	"user_reserva_dispo_api/models"
 	"user_reserva_dispo_api/utils/errors"
 )
 
@@ -16,7 +17,8 @@ type reservaService struct{}
 
 type reservaServiceInterface interface {
 	ParseDate(dateString string) (time.Time, errors.ApiError)
-	ComprobaDispReserva(reservationDto dtos.ReservationAvailabilityDto) (time.Time, errors.ApiError)
+	ComprobaDispReserva(reservationDto dtos.ReservationAvailabilityDto) errors.ApiError
+	PostReserva(reservationDto dtos.CreateReservationDto) (dtos.CreateReservationDto, errors.ApiError)
 }
 
 var (
@@ -38,17 +40,67 @@ func (s *reservaService) ParseDate(dateString string) (time.Time, errors.ApiErro
 	return date, nil
 }
 
-func (s *reservaService) ComprobaDispReserva(reservationDto dtos.ReservationAvailabilityDto) (time.Time, errors.ApiError) {
+func (s *reservaService) ComprobaDispReserva(reservationDto dtos.ReservationAvailabilityDto) errors.ApiError {
 	hotel, err := GetHotelById(reservationDto.HotelID)
+
 	if err != nil {
 		fmt.Println("Error parsing date:", err)
-		return time.Now(), errors.NewBadRequestApiError(err.Error())
+		return errors.NewBadRequestApiError(err.Error())
 	}
-	log.Println(hotel.ID)
-	reservas, err := daos.CheckAvailability(reservationDto.HotelID, reservationDto.StartDate, reservationDto.EndDate)
-	log.Println(reservas)
 
-	return time.Now(), nil
+	reservas, err := daos.CheckAvailability(reservationDto.HotelID, reservationDto.StartDate, reservationDto.EndDate)
+	if err != nil {
+		return err
+	}
+	var listaDias []time.Time
+	for i := reservationDto.StartDate; i.Before(reservationDto.EndDate) || i.Equal(reservationDto.EndDate); i = i.AddDate(0, 0, 1) {
+		listaDias = append(listaDias, i)
+	}
+
+	return comprobar(reservas, hotel.RoomCount, listaDias)
+}
+
+func (s *reservaService) PostReserva(reservationDto dtos.CreateReservationDto) (dtos.CreateReservationDto, errors.ApiError) {
+	var compReserva dtos.ReservationAvailabilityDto
+	compReserva.StartDate = reservationDto.StartDate
+	compReserva.EndDate = reservationDto.EndDate
+	compReserva.HotelID = reservationDto.HotelID
+	err := s.ComprobaDispReserva(compReserva)
+
+	if err != nil {
+		return reservationDto, err
+	}
+	var reserva models.Reservation
+	reserva.HotelID = reservationDto.HotelID
+	reserva.EndDate = reservationDto.EndDate
+	reserva.StartDate = reservationDto.StartDate
+	reserva.UserID = reservationDto.UserID
+
+	reserva, err = daos.CreateReservation(reserva)
+
+	if err != nil {
+		return reservationDto, err
+	}
+	reservationDto.ReservationID = reserva.ReservationID
+
+	return reservationDto, nil
+}
+
+func comprobar(reservas models.Reservations, habitacionesTotales int, listaDias []time.Time) errors.ApiError {
+	conteoDias := make([]int, len(listaDias))
+	for c, dia := range listaDias {
+		for _, reserva := range reservas {
+			if reserva.StartDate.Before(dia.AddDate(0, 0, -1)) && reserva.EndDate.After(dia.AddDate(0, 0, 1)) {
+				conteoDias[c]++
+				if conteoDias[c] >= habitacionesTotales {
+					return errors.NewBadRequestApiError(fmt.Sprintf("El dia %d/%d/%d no hay disponibilidad", listaDias[c].Day(), int(listaDias[c].Month()), listaDias[c].Year()))
+
+				}
+			}
+		}
+	}
+
+	return nil
 }
 
 func GetHotelById(id string) (dtos.HotelDto, errors.ApiError) {
